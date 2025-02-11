3f6ec7557cd13f5a0ba1b4dd859212d2c7c1b6f2
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-02-11 09:22:00 -0300

    Add files via upload

---
+++ Vetor_Array2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testesjava1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Vetor_Array2 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double[] notas = new double [2];
+        
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Digite a " + (i + 1) + " nota: ");
+            notas[i] = ler.nextDouble();
+        }
+        
+        System.out.println("\nExibindo os Resultados: ");
+        for (double nota : notas) {
+            System.out.println("Nota: " + nota);
+        }
+        
+        ler.close();
+    }
+            
+}
