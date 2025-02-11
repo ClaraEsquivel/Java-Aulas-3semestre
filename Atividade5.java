39af97c4ae9210a97c809b0317c8ae1054432554
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-02-11 09:11:42 -0300

    Add files via upload

---
+++ Atividade5.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testesjava1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author aluno.den
+ */
+public class Atividade5 {
+
+    public static void main(String[] args) {
+        
+        Scanner notas =  new Scanner(System.in);
+        
+        String nome, resultado;
+        double primeiraNota, segundaNota, media;
+        
+        
+        System.out.println("Digite o nome do Aluno: ");
+        nome = notas.next();
+        
+        
+        do{
+            System.out.println("Digite a primeira nota: ");
+            primeiraNota = notas.nextDouble();
+            
+            System.out.println("A nota não pode ser menor que 0 ou maior que 10!");    
+        } while(primeiraNota < 0 || primeiraNota > 10);
+        
+        
+        do{
+           System.out.println("Digite a segunda nota: ");
+           segundaNota = notas.nextDouble(); 
+           
+           System.out.println("A nota não pode ser menor que 0 ou maior que 10!");    
+        } while (segundaNota < 0 || segundaNota > 10);
+        
+
+
+        media = (primeiraNota + segundaNota) / 2;
+        
+        
+        if (media >= 7){
+            resultado = "Aprovado";
+        } else if (media >= 5){
+            resultado = "Recuperação";
+        } else{
+            resultado = "Reprovado";
+        }
+        
+        
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Média: " + media);
+        System.out.println("Resultado: " + resultado);
+        
+    }
+}
---
+++ For.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testesjava1;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class For {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 5; i++){
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
