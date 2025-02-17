60d7bdc3a9e391627aadff3aa1c23cc4ef61bdfa
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-02-17 11:27:26 -0300

    Add files via upload

---
+++ encapsulamento4/Livro.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.encapsulamento4;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ISBN;
+    private int numeroPaginas;
+    private double preco;
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, int ISBN, int numeroPaginas, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.numeroPaginas = numeroPaginas;
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    
+}
