bd96f9fe6129512c1f9a75285d8038b60009e447
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-03-25 11:15:36 -0300

    Add files via upload

---
+++ aula_5.1_relacionamento/aula_5_relacionamento/src/main/java/com/senai/aula_5_relacionamento/Aula5RelacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.aula_5_relacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula5RelacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula5RelacionamentoApplication.class, args);
+	}
+
+}
